d1414351e94e05fed4dd987884d3e96ee05cd5dc
Rosy Chadha <rosychadha2018@gmail.com>
2021-09-04 18:57:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function valuecal(result){
+    calculatorForm.evalresult.value = calculatorForm.evalresult.value + result;
+}
+
+function AllClear(){
+    calculatorForm.evalresult.value = " ";
+}
+
+function percentage(result) {
+    calculatorForm.evalresult.value = calculatorForm.evalresult.value/"100";
+}
